adf51179e680915c6c09f9a2aac2916444817e38
hossameldeen <hossamabdalh@gmail.com>
2018-05-30 17:39:48 +0200

    test forms

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  sampleForm;
 
   constructor() { }
 
   ngOnInit() {
+    this.sampleForm = new FormGroup({
+      'username': new FormControl(),
+      'password': new FormControl(),
+    });
+  }
+
+  save() {
+    let username = this.sampleForm.controls.username.value;
+    let password = this.sampleForm.controls.password.value;
+    console.log("form submitted with username and password as "+username+", "+password);
   }
 
 }
