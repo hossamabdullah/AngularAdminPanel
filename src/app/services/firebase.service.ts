b2faf89821b3f48f3879042c9c4b7c7ad07c0456
Hossameldeen <hossamabdalh@gmail.com>
2018-06-27 14:16:25 +0200

    adding class firebase

---
+++ src/app/services/firebase.service.ts
@@ -0,0 +1,12 @@
+import {Injectable} from '@angular/core';
+import { User } from '../types/user.model';
+import { Asset } from '../types/asset.model';
+import { Http } from '@angular/http';
+import { AssetTransfer } from '../types/assetTransfer.model';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class FirebaseService{
+    
+}
