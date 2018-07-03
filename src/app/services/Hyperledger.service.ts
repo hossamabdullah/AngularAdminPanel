ccbfa988965889e1892b167ef46a2e3a3e734196
Hossameldeen <hossamabdalh@gmail.com>
2018-07-03 19:18:35 +0200

    update call percentage

--- src/app/services/Hyperledger.service.ts
+++ src/app/services/Hyperledger.service.ts
@@ -77,12 +77,17 @@ export class HyperledgerService {
 
     /******/
     
-    callPercentage(assetIds: String[]){
+    callPercentage(assetIds: string[], type: string){
         let alllength = 0;
         this.http.get('http://173.193.79.254:31090/api/Commodity').subscribe(
             (response)=>{
                 console.log(response.json().length)
-                alllength = response.json().length
+                let assets: Asset[] = response.json()
+                assets.forEach((asset, index, array)=>{
+                    if(asset.type == type){
+                        alllength = alllength + 1;
+                    }
+                })
             }
             ,(error)=> console.log(error)
         )
